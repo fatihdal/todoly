6c8a5442d745d2972299abe42d89e309695058ab
fatihdal <ffatihdal@gmail.com>
2020-11-22 20:59:01 +0300

    [#25] Setup local testing environment

--- src/test/java/dal/fatih/todoly/TodolyTest.java
+++ src/test/java/dal/fatih/todoly/TodolyTest.java
@@ -1,7 +1,16 @@
 package dal.fatih.todoly;
 
+import org.junit.Test;
+
 public class TodolyTest {
 
+    @Test
+    public void shouldShowFilteringTaskByNameAndDescriptionWithMenuIndex6(){
 
+    }
 
+    @Test
+    public void shouldNotSupportFilteringTasksByNameAndDescriptionYet(){
+
+    }
 }
