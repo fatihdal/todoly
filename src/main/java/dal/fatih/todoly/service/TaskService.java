790d910569a4d79a2a3cd7d47387617812eadb50
fatihdal <ffatihdal@gmail.com>
2021-04-14 01:31:24 +0300

    [#33] Created TaskService.java interface

---
+++ src/main/java/dal/fatih/todoly/service/TaskService.java
@@ -0,0 +1,22 @@
+package dal.fatih.todoly.service;
+
+import dal.fatih.todoly.Task;
+import dal.fatih.todoly.dto.TaskDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TaskService {
+
+    TaskDTO handleCreateTask();
+
+    List<TaskDTO> listAllTasks();
+
+    TaskDTO get(Long id);
+
+    boolean delete(Long id);
+
+    List<Task> filterByDueDate(LocalDateTime dueDate);
+
+    List<Task> filterByTitleOrDescription(String keyword);
+}
