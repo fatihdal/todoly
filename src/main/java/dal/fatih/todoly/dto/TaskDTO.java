282b0ba719f433ea386deec7ad68509b086dfe17
fatihdal <ffatihdal@gmail.com>
2021-04-14 01:17:40 +0300

    [#33] Created TaskDTO.java class

---
+++ src/main/java/dal/fatih/todoly/dto/TaskDTO.java
@@ -0,0 +1,94 @@
+package dal.fatih.todoly.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class TaskDTO {
+
+    private Long id;
+
+    @NotNull(message = "Title must not be empty")
+    @Size(min = 5, max = 120, message = "Title length must be between 5 and 120")
+    private String title;
+
+    private String description;
+
+    @NotNull(message = "Due Date must not be empty")
+    @Future(message = "Due date must be a future date")
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime dueDate;
+
+    public TaskDTO() {
+    }
+
+    public TaskDTO(Long id, String title, String description, LocalDateTime dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    public TaskDTO(String title, String description, LocalDateTime dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TaskDTO)) return false;
+        TaskDTO taskDTO = (TaskDTO) o;
+        return getId().equals(taskDTO.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return " Task Id = " + id +
+                " Title = " + title +
+                " Description = " + description +
+                " Due Date = " + dueDate;
+    }
+}
